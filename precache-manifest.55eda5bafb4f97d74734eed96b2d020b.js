2939e8cf527a496eee1dd1d1a76a15bf0ffb115b
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-08-07 00:01:36 -0400

    deploy

---
+++ precache-manifest.55eda5bafb4f97d74734eed96b2d020b.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd93c411fd02a1e1f31b6a58f810e605",
+    "url": "/evocate-public/index.html"
+  },
+  {
+    "revision": "fd3c6d0e3f394a741c54",
+    "url": "/evocate-public/static/css/2.bb083c27.chunk.css"
+  },
+  {
+    "revision": "f3940b803569ad2e5c42",
+    "url": "/evocate-public/static/css/main.f38e5c95.chunk.css"
+  },
+  {
+    "revision": "fd3c6d0e3f394a741c54",
+    "url": "/evocate-public/static/js/2.3b04cb33.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/evocate-public/static/js/2.3b04cb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3940b803569ad2e5c42",
+    "url": "/evocate-public/static/js/main.d241ea3f.chunk.js"
+  },
+  {
+    "revision": "4f6f76544d0dc80efdf1",
+    "url": "/evocate-public/static/js/runtime-main.0f2bd245.js"
+  },
+  {
+    "revision": "801f5573a23d9c4eaa47ecc44a765f39",
+    "url": "/evocate-public/static/media/Aquaman.801f5573.png"
+  },
+  {
+    "revision": "e9ac2c209a00f2be3e4f6bc6c1cf985d",
+    "url": "/evocate-public/static/media/Arbok.e9ac2c20.jpg"
+  },
+  {
+    "revision": "a1d70c5057a75338928a4cadcb051ba0",
+    "url": "/evocate-public/static/media/Ash.a1d70c50.jpeg"
+  },
+  {
+    "revision": "eac3ae2f709f04df291c7524aef29742",
+    "url": "/evocate-public/static/media/Batgirl.eac3ae2f.png"
+  },
+  {
+    "revision": "fdf64cbbbe4c7b95c196e1d77acd796c",
+    "url": "/evocate-public/static/media/Batman.fdf64cbb.jpg"
+  },
+  {
+    "revision": "5874d5fcdf6fd0a3ae6f53df21e2d4c9",
+    "url": "/evocate-public/static/media/BlackPanther.5874d5fc.png"
+  },
+  {
+    "revision": "fc4ffe3276751d707a32a2cfdb06cb0e",
+    "url": "/evocate-public/static/media/BlackWidow.fc4ffe32.jpg"
+  },
+  {
+    "revision": "85a70a2961256d1002e8acc0b70dbb3c",
+    "url": "/evocate-public/static/media/Bulbasaur.85a70a29.png"
+  },
+  {
+    "revision": "18de264c17d52b845a45a46dd2b8fca7",
+    "url": "/evocate-public/static/media/CaptainAmerica.18de264c.jpg"
+  },
+  {
+    "revision": "bf204142e858948f9614e7f346d1a646",
+    "url": "/evocate-public/static/media/CaptainMarvel.bf204142.jpg"
+  },
+  {
+    "revision": "7f5c43af506586f18de11cf9f3a0fba4",
+    "url": "/evocate-public/static/media/Charmeleon.7f5c43af.jpg"
+  },
+  {
+    "revision": "83aff3003a1381d4c99f9558d6eb642e",
+    "url": "/evocate-public/static/media/Charzard.83aff300.jpg"
+  },
+  {
+    "revision": "9474fe7b1c572c8621ef3f9a133cebda",
+    "url": "/evocate-public/static/media/Ditto.9474fe7b.jpg"
+  },
+  {
+    "revision": "5d35c6a53dd70ac0dc68e99beb4887bd",
+    "url": "/evocate-public/static/media/Dragonair.5d35c6a5.jpg"
+  },
+  {
+    "revision": "19e2c08eb1f0c6ccf5e65d274d3d34b9",
+    "url": "/evocate-public/static/media/Evee.19e2c08e.jpg"
+  },
+  {
+    "revision": "b617dc209ee5b000759625a5161f18a0",
+    "url": "/evocate-public/static/media/Flareon.b617dc20.png"
+  },
+  {
+    "revision": "52b2d90f25ddc3fb22fd22434bb532d8",
+    "url": "/evocate-public/static/media/Flash.52b2d90f.jpg"
+  },
+  {
+    "revision": "c3207aecd6916d7860d68c63a2d87918",
+    "url": "/evocate-public/static/media/GreenLantern.c3207aec.jpg"
+  },
+  {
+    "revision": "5d6842e992504f6e3e47d66656474492",
+    "url": "/evocate-public/static/media/Hulk.5d6842e9.jpg"
+  },
+  {
+    "revision": "28b66a5cf4a961685a334d6268a2fa4c",
+    "url": "/evocate-public/static/media/IronMan.28b66a5c.jpg"
+  },
+  {
+    "revision": "74d8baedf29e7ac873f99efe992b1db7",
+    "url": "/evocate-public/static/media/Magikarp.74d8baed.png"
+  },
+  {
+    "revision": "e1e2466f7368f34263781757833e6721",
+    "url": "/evocate-public/static/media/Meowth.e1e2466f.png"
+  },
+  {
+    "revision": "fdad4d273335a780e0b0e48a7a305522",
+    "url": "/evocate-public/static/media/Persian.fdad4d27.png"
+  },
+  {
+    "revision": "baa610f772070869ef6cfd15b0173c10",
+    "url": "/evocate-public/static/media/Pikachu.baa610f7.jpg"
+  },
+  {
+    "revision": "1525d19a8809e7224db41883d70b1c60",
+    "url": "/evocate-public/static/media/Psyduck.1525d19a.png"
+  },
+  {
+    "revision": "0a090d646f36531e8db9f8de03b20058",
+    "url": "/evocate-public/static/media/Robin.0a090d64.jpg"
+  },
+  {
+    "revision": "e032236d83c74c09a5a446b187f43a14",
+    "url": "/evocate-public/static/media/SandSlash.e032236d.jpg"
+  },
+  {
+    "revision": "9609b27efed96965307a006a35d7be2e",
+    "url": "/evocate-public/static/media/Snorlax.9609b27e.png"
+  },
+  {
+    "revision": "c27b516b7f7d92701580c2be77927b12",
+    "url": "/evocate-public/static/media/SpiderWoman.c27b516b.png"
+  },
+  {
+    "revision": "7824ef98f3453988832a027c9db8a1c1",
+    "url": "/evocate-public/static/media/Spiderman.7824ef98.jpg"
+  },
+  {
+    "revision": "cc003048a8a6b7dda36652c994d1288a",
+    "url": "/evocate-public/static/media/Squirtle.cc003048.jpg"
+  },
+  {
+    "revision": "9592edf222307d36ec674e69b24c9ff7",
+    "url": "/evocate-public/static/media/SuperMan.9592edf2.jpg"
+  },
+  {
+    "revision": "60c9039cacaac188911b934eed459915",
+    "url": "/evocate-public/static/media/Thor.60c9039c.jpg"
+  },
+  {
+    "revision": "7557408be3c943450140e2aedeeafc9f",
+    "url": "/evocate-public/static/media/Wolverine.7557408b.jpg"
+  },
+  {
+    "revision": "ca9f94a922a9446e1b2e227740fe8d6c",
+    "url": "/evocate-public/static/media/WonderWoman.ca9f94a9.jpg"
+  },
+  {
+    "revision": "32cb324551b3d7eeccf5bbd56c737f5c",
+    "url": "/evocate-public/static/media/cardback.32cb3245.png"
+  },
+  {
+    "revision": "aca750302133b6541647a16f640a3769",
+    "url": "/evocate-public/static/media/cat.aca75030.jpg"
+  },
+  {
+    "revision": "7b1e36f0ac316dd62d219332827c2f7e",
+    "url": "/evocate-public/static/media/random.7b1e36f0.jpg"
+  },
+  {
+    "revision": "cd7bb6bec60f6799aa174b4e07b92ea4",
+    "url": "/evocate-public/static/media/react.cd7bb6be.png"
+  }
+]);
